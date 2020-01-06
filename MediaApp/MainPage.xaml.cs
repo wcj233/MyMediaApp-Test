14a6eb252a58f463188c65d33101398ac81e01fd
Fay Wang <v-faywan@microsoft.com>
2020-01-06 10:03:49 +0800

    Add project files.

---
+++ MediaApp/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using FFmpegInterop;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MediaApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            
+        }
+
+        //public MediaSource VideoSource { get; set; }
+
+       
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker picker = new FileOpenPicker();
+            picker.FileTypeFilter.Add("*");
+
+            var file = await picker.PickSingleFileAsync();
+
+            var stream = await file.OpenReadAsync();
+
+            var mediaStreamsource = FFmpegInteropMSS.CreateFFmpegInteropMSSFromStream(stream, false, false).GetMediaStreamSource();
+
+            mediaPlayerElement.Source = MediaSource.CreateFromMediaStreamSource(mediaStreamsource);
+            
+
+
+        }
+
+        
+    }
+}
